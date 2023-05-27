de1495e24c5caf55b1a236d59bdf0c335199f043
Dhonatas Jesus <dhonatascross@gmail.com>
2023-05-27 19:05:22 -0300

    Update class exercises

--- class_exercises/03.py
+++ class_exercises/03.py
@@ -1,16 +1,32 @@
+class Retangulo:
+    def __init__(self, base, altura, ):
+        self.base = base
+        self.altura = altura
+
+    def muda_lado(self, nova_base, nova_altura):
+        self.base = nova_base
+        self.altura = nova_altura
+
+    def valor_lados(self):
+        return self.base, self.altura
+
+    def calc_area(self):
+        return (self.base * self.altura) / 2
+
+    def calc_perimetro(self):
+        return 2 * (base + altura)
 
 
+base = float(input("Informe a vertical do local: "))
+altura = float(input("Informe a horizontal do local: "))
 
 
+retangulo = Retangulo(base, altura)
+
+area = retangulo.calc_area()
+perimentro = retangulo.calc_perimetro()
+
+print(f"Sera necessario {area} metros quadrados de piso")
+print(f"Sera necessario {perimentro} metros quadrados de rodape")
 
 
-
-
-
-# Classe Retangulo: Crie uma classe que modele um retangulo:
-
-# Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
-# Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
-# Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidades 
-# de um local. Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de 
-# rodapés necessárias para o local.
---
+++ class_exercises/04.py
@@ -0,0 +1,27 @@
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Classe Pessoa: Crie uma classe que modele uma pessoa:
+
+# Atributos: nome, idade, peso e altura
+# Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa 
+# envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
